1e835de76609291081f43ef987942a411093e17e
unknown <honorpurpose@gmail.com>
2019-04-27 13:57:15 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const axios = require("axios");
+require('dotenv').config();
+
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/api/test', (req,res,next) => {
+    res.send("This worked!")
+})
+
+
+
+const port = process.env.PORT || 8060;
+app.listen(port, ()=>{
+    console.log(`running on port ${port}`)
+})
