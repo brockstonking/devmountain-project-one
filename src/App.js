72e6bd38dc0408e83be6091e96cafb1ae3db260d
unknown <honorpurpose@gmail.com>
2019-04-27 14:10:15 -0600

    working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import url from './api'
-const axios = require('axios')
+import axios from 'axios'
 
 
 class App extends Component {
