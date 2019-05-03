792f197b2240612b0e1038250cb49cc0b156730f
unknown <honorpurpose@gmail.com>
2019-05-02 22:15:03 -0600

    working

--- src/components/display/sidebar/favorites/favorites.js
+++ src/components/display/sidebar/favorites/favorites.js
@@ -10,13 +10,18 @@ class Favorites extends Component {
         let question = this.props.question;
         let answer = this.props.answer;
         return(
-            <div className='favoriteList'>
-                <div className='editDelete'>
-                    <img onClick={ () => { this.props.edit(this.props.questionId) } } src="https://cdn3.iconfinder.com/data/icons/web-ui-3/128/Compose-2-512.png" className='editButton' alt=""/>
-                    <img onClick={ () => {this.props.delete(this.props.questionId)} } className='deleteButton' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQivHiJB2Hond5R2oMSz0nUWVLgw-NjTv74Dad5zVdWgcq2rk6C" alt=""/> 
-                </div>                
-                <p>{ question }</p>
-                <p>{ answer }</p>
+            <div>
+                <div>
+                    
+                </div>
+                <div className='favoriteList'>
+                    <div className='editDelete'>
+                        <img onClick={ () => { this.props.edit(this.props.questionId) } } src="https://cdn3.iconfinder.com/data/icons/web-ui-3/128/Compose-2-512.png" className='editButton' alt=""/>
+                        <img onClick={ () => {this.props.delete(this.props.questionId)} } className='deleteButton' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQivHiJB2Hond5R2oMSz0nUWVLgw-NjTv74Dad5zVdWgcq2rk6C" alt=""/> 
+                    </div>                
+                    <p>{ question }</p>
+                    <p>{ answer }</p>
+                </div>
             </div>
         )
     }
